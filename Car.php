b9f8707d9cb47df7d3234e24e7b6994c026e1ce5
Adam Won & Kevin Tokheim <student@epicodus.com>
2015-08-03 14:32:25 -0700

    Car Pictures/Getters and Setters.

--- Car.php
+++ Car.php
@@ -4,75 +4,76 @@
     private $make_model;
     private $price;
     private $miles;
-    private $car;
+    private $image_path;
 
-
-    function __construct($make_model, $price, $miles, $car)
+    function __construct($make_model, $price, $miles, $image_path)
     {
 
         $this->model = $make_model;
         $this->cost = $price;
-        $this->milage = $miles;
-        $this->vehicle = $car;
+        $this->mileage = $miles;
+        $this->image = $image_path;
 
     }
 
-
     function setPrice($new_price)
     {
-      $float_price = (float) $new_price;
-      if ($float_price !=0){
-          $this->price = $float_price;
+
+        $float_price = (float) $new_price;
+        if ($float_price != 0){
+          $formatted_price = number_format($float_price, 2);
+          $this->cost = $formatted_price;
+        }
+    }
+
+    function setModel($new_model)
+    {
+          $this->model = $new_model;
+
+    }
+
+    function setMileage($new_miles)
+    {
+          $this->mileage = $new_miles;
+    }
+
+    function setImage($new_image)
+    {
+          $this->image = $new_image;
     }
 
     function getPrice()
     {
-        return $this->price;
-
+          return $this->cost;
     }
 
-    }
-
-    function setMiles($new_miles)
-
+    function getModel()
     {
+          return $this->model;
+    }
 
-        $this->miles = ($new_miles);
+    function getMileage()
+    {
+          return $this->miles;
+    }
 
+    function getImage()
+    {
+          return $this->image;
     }
 
   }
 
-    $porsche = new Car();
-    $porsche->make_model = "2014 Porsche 911";
-    $porsche->price = 114991;
-    $porsche->miles = 7864;
-
-    $ford = new Car();
-    $ford->make_model = "2011 Ford F450";
-    $ford->price = 55995;
-    $ford->miles = 14241;
-
-    $lexus = new Car();
-    $lexus->make_model = "2013 Lexus RX 350";
-    $lexus->price = 44700;
-    $lexus->miles = 20000;
-
-    $mercedes = new Car();
-    $mercedes->make_model = "Mercedes Benz CLS550";
-    $mercedes->price = 39900;
-    $mercedes->miles = 37979;
-
-    $nissan = new Car();
-    $nissan->make_model = "2010 Nissan Versa";
-    $nissan->price = 4000;
-    $nissan->miles = 128000;
+    $porsche = new Car("2014 Porsche 911", 114991, 7864, "car/porsche.jpg");
+    $ford = new Car("2011 Ford F450", 55995, 14241, "car/ford.jpg");
+    $lexus = new Car("2013 Lexus RX 350", 44700, 20000, "car/lexus.jpg");
+    $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "car/mercedes.jpg");
 
     $cars = array($porsche, $ford, $lexus, $mercedes);
 
     $cars_matching_search = array();
     foreach ($cars as $car) {
-      if ($car->price < $_GET["price"]) {
+      if ($car->cost < $_GET["price"]) {
         array_push($cars_matching_search, $car);
       }
     }
@@ -89,11 +90,15 @@
   <ul>
     <?php
       foreach ($cars_matching_search as $car) {
-        $car_prices= $car->getPrice();
-        echo "<li> $car->make_model </li>";
+        $car_image = $car->getImage();
+        $car_price = $car->getPrice();
+        $car_model = $car->getModel();
+        $car_mileage = $car->getMileage();
+        echo "<li> $car->model </li>";
+        echo "<div><img src='$car->image'> </div>";
         echo "<ul>";
-              echo "<li> $$car->price </li>";
-              echo "<li> Miles: $car->miles </li>";
+              echo "<li> $$car->cost </li>";
+              echo "<li> Miles: $car->mileage </li>";
         echo "</ul>";
         }
     ?>
